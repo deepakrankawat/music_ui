49f9b038da7098190405309fe2462130839121b4
deepakrankawat <118287211+deepakrankawat@users.noreply.github.com>
2025-05-30 11:34:17 +0530

    info_screen

--- lib/Service_page/lyrics_writing_page.dart
+++ lib/Service_page/lyrics_writing_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:music/MVVM/service_card.dart';
 import 'package:music/MVVM/service_model.dart';
-
 
 class DetailScreen extends StatelessWidget {
   final ServiceModel service;
@@ -11,8 +11,50 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(service.title)),
-      body: Center(child: ServiceCard(service: service)),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset(service.image2, fit: BoxFit.cover),
+          ),
+          Container(
+            width: double.infinity,
+            color: Colors.black.withOpacity(0.65),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              Card(elevation: 10,
+              shadowColor: Colors.blueAccent,
+                child: Padding(
+                padding: const EdgeInsets.all(40),
+                child: Column(
+                  children: [
+                    Image.asset(service.image1,),
+                    Column(children: [Text(service.title , style: GoogleFonts.syne(fontSize: 30),),
+                    Text(service.subtitle)],)
+                  ],
+                ),
+              ))
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
